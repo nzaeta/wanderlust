5159bf89a91a1fe20e4d914f992a3cf32ce1a918
nzaeta <nicocpla@hotmail.com>
2023-08-25 20:08:34 -0300

    [OK] Config Files Added

---
+++ Config/src/test/java/com/nicoz/NZWanderlust/NzWanderlustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoz.NZWanderlust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzWanderlustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
