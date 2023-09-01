7e7cd8a7d00185b88fcf02c990b1e1b1c64f671a
nzaeta <nicocpla@hotmail.com>
2023-09-01 18:05:22 -0300

    [OK] deleted buyer and seller

--- Models/Repositories/BuyerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nicoz.NZWanderlust.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.nicoz.NZWanderlust.Entities.Buyer;
-
-
-public interface BuyerRepository extends JpaRepository<Buyer, Integer>{
-
-}
--- Models/Repositories/SellerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nicoz.NZWanderlust.Repositories;
-
-import com.nicoz.NZWanderlust.Entities.Seller;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellerRepository extends JpaRepository<Seller, Long> {
-}
